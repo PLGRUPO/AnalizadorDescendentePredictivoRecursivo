f48b3b974c380f36e7687931699c6b3369b1698c
Sergio M. Afonso Fumero <alu0100700459@ull.edu.es>
2014-03-06 20:00:39 +0000

    Estructura de tests reparada para poder ejecutarlos

--- public/js/tests.js
+++ public/js/tests.js
@@ -1,10 +1,6 @@
-var assert = chai.assert,
-    expect = chai.expect,
-    should = chai.should(); // Note that should has to be executed
+var assert = chai.assert;
 
-
-
-var foobar = {
+/*var foobar = {
   inputOriginal: function() {
   	        var prev = localStorage.fileInput;
         parseInput('[Cabecera1]\nentrada  = valor\n; Comentarios\n\n[Cabecera2]\n;no hay valores');
@@ -27,8 +23,8 @@ var foobar = {
   		}
   }
   
-};
-/*
+};*/
+
 suite('Ficheros .ini', function() {
 	  
 	  test('return true -> Url correcta', function () {   
@@ -39,21 +35,14 @@ suite('Ficheros .ini', function() {
         assert.deepEqual(foobar.localStore(),'true');
     });
     
-
-    
     test('Parseado correcto de una cadena', function () {
-        var prev = localStorage.fileInput;
         parseInput('[Cabecera1]\nentrada  = valor\n; Comentarios\n\n[Cabecera2]\n;no hay valores');
         assert.deepEqual(foobar.inputOriginal(), '<ol>\n\n\t  <li> <span class="Header"> {\n  "type": "Header",\n  "match": [\n    "[Cabecera1]",\n    "Cabecera1"\n  ]\n} </span>\n\t\n\t  </li><li> <span class="Blancos"> {\n  "type": "Blancos",\n  "match": [\n    "\\n"\n  ]\n} </span>\n\t\n\t  </li><li> <span class="Igualdad"> {\n  "type": "Igualdad",\n  "match": [\n    "entrada  = valor",\n    "entrada",\n    "valor"\n  ]\n} </span>\n\t\n\t  </li><li> <span class="Blancos"> {\n  "type": "Blancos",\n  "match": [\n    "\\n"\n  ]\n} </span>\n\t\n\t  </li><li> <span class="Comentarios"> {\n  "type": "Comentarios",\n  "match": [\n    "; Comentarios"\n  ]\n} </span>\n\t\n\t  </li><li> <span class="Blancos"> {\n  "type": "Blancos",\n  "match": [\n    "\\n\\n"\n  ]\n} </span>\n\t\n\t  </li><li> <span class="Header"> {\n  "type": "Header",\n  "match": [\n    "[Cabecera2]",\n    "Cabecera2"\n  ]\n} </span>\n\t\n\t  </li><li> <span class="Blancos"> {\n  "type": "Blancos",\n  "match": [\n    "\\n"\n  ]\n} </span>\n\t\n\t  </li><li> <span class="Comentarios"> {\n  "type": "Comentarios",\n  "match": [\n    ";no hay valores"\n  ]\n} </span>\n\t</li></ol>');
-        localStorage.fileInput = prev;
     });
     
     test('Parseado correcto de una cadena 1', function () {
-        var prev = localStorage.fileInput;
         parseInput('[Cabecera1]\nentrada  = valor\n; Comentarios\n\n[Cabecera2]\n;no hay valores');
-        assert.deepEqual(foobar.inputOriginal(), '<ol>\n\n\t  <li> <span class="Header"> {\n  "type": "Header",\n  "match": [\n    "[Cabecera1]",\n    "Cabecera1"\n  ]\n} </span>\n\t\n\t  </li><li> <span class="Blancos"> {\n  "type": "Blancos",\n  "match": [\n    "\\n"\n  ]\n} </span>\n\t\n\t  </li><li> <span class="Igualdad"> {\n  "type": "Igualdad",\n  "match": [\n    "entrada  = valor",\n    "entrada",\n    "valor"\n  ]\n} </span>\n\t\n\t  </li><li> <span class="Blancos"> {\n  "type": "Blancos",\n  "match": [\n    "\\n"\n  ]\n} </span>\n\t\n\t  </li><li> <span class="Comentarios"> {\n  "type": "Comentarios",\n  "match": [\n    "; Comentarios"\n  ]\n} </span>\n\t\n\t  </li><li> <span class="Blancos"> {\n  "type": "Blancos",\n  "match": [\n    "\\n\\n"\n  ]\n} </span>\n\t\n\t  </li><li> <span class="Header"> {\n  "type": "Header",\n  "match": [\n    "[Cabecera2]",\n    "Cabecera2"\n  ]\n} </span>\n\t\n\t  </li><li> <span class="Blancos"> {\n  "type": "Blancos",\n  "match": [\n    "\\n"\n  ]\n} </span>\n\t\n\t  </li><li> <span class="Comentarios"> {\n  "type": "Comentarios",\n  "match": [\n    ";no hay valores"\n  ]\n} </span>\n\t</li></ol>');
-        localStorage.fileInput = prev;
+        assert.deepEqual(foobar.inputOriginal(), '');
     });
     
 });
- */
